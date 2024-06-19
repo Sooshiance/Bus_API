ebfcceb99f32d321210243eae8c914bc04ac531b
sooshiance <s.katafract@gmail.com>
2024-06-19 19:04:11 +0330

    add basic options for dj_ecommerce

---
+++ api/dj/ecommerce.py
@@ -0,0 +1,33 @@
+from flask import Flask, request, jsonify
+import requests
+
+
+app = Flask(__name__)
+
+
+# TODO : If owner wants to check which product is not in the store
+@app.route('/active-product/', methods=['GET'])
+def getActiveProduct():
+    pass
+
+
+# TODO : If owner wants to check his daily transactions
+@app.route('/daily-transactions/', methods=['GET'])
+def dailyTransactions():
+    pass
+
+
+# TODO : If owner wants to check all messages from his/her customers
+@app.route('/read-comments/', methods=['GET'])
+def readComments():
+    pass
+
+
+# TODO : If owner wants to combine all functions above and needs a report
+@app.route('/daily-report/', methods=['GET'])
+def generateJournal():
+    pass
+
+
+if __name__ == "__main__":
+    app.run(host="http://127.0.0.1", port=5000, debug=True)
